70b2b2b2ec789934bebc0487fbd4816939f30900
BenPyton <omegazed97@gmail.com>
2020-09-27 20:49:07 +0200

    Updated version number

--- Utils.cpp
+++ Utils.cpp
@@ -28,7 +28,7 @@
 
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 0
-#define BUGFIX_VERSION 4
+#define BUGFIX_VERSION 5
 
 const QString Utils::AppName = "Sound Generator";
 const QString Utils::CompanyName = "Benoit Pelletier";
